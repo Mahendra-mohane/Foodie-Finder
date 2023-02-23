7efd1aba3bdf0c81464b036880cab2b6cf5a2521
AAdi23456 <118712556+AAdi23456@users.noreply.github.com>
2023-02-23 23:00:03 +0530

    done

---
+++ data_base/data_base.js
@@ -0,0 +1,464 @@
+let alldata={
+breakfast:[
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0000_Stack620_0000_CFA_1605_60_Biscuit_Chicken_PROD_2155_1240px.png",
+      "title": "Chick-fil-A® Chicken Biscuit",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0019_%5BFeed%5D_0000s_0019_Breakfast_Biscuit-Spicy-Chicken.png",
+      "title": "Spicy Chicken Biscuit",
+      "desc": "Chick-fil-A® Chicken Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0014_%5BFeed%5D_0000s_0024_Breakfast_Chicken-Mini-4ct.png",
+      "title": "Chick-fil-A Chick-n-Minis™",
+      "desc": "460 Cal per Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0039_Stack620_0011_35592_eggWHTchk_p1_trg_42016_1240px.png",
+      "title": "Egg White Grill",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0004_ChickenSausageBurrito-pdp_1797v2_2880px_resize2.png",
+      "title": "Hash Brown Scramble Burrito",
+      "desc": "Spicy Chicken Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0005_ChickenHashbrownBreakfastBowl-pdp_Hearty-Breakfast-Bowl_resize2.png",
+      "title": "Hash Brown Scramble Bowl",
+      "desc": "430 Cal per biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Breakfast/Biscuit_ChickenEggCheese_PROD_2182_rev17_RGB_1080px.png",
+      "title": "Chicken, Egg & Cheese Biscuit",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0008_Bacon-Egg-Cheese-Biscuit_620_PDP.png",
+      "title": "Bacon, Egg & Cheese Biscuit",
+      "desc": "Chick-fil-A Chick-n-Minis™"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0026_%5BFeed%5D_0000s_0012_Breakfast_Sausage-Egg-Cheese-Biscuit.png",
+      "title": "Sausage, Egg & Cheese Biscuit",
+      "desc": "360 Cal per 4 Chick-n-Minis"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Breakfast/CECMuffin_1080px.png",
+      "title": "Chicken, Egg, & Cheese Muffin",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0007_BaconEggCheeseMuffin-Feed_0001s_0008_CFA_1605_59_EnglishMuffin_BaconEggCheese_PROD_2121_1240px.png",
+      "title": "Bacon, Egg & Cheese Muffin",
+      "desc": "Egg White Grill"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0036_%5BFeed%5D_0000s_0002_Breakfast_Sausage-Egg-Cheese-Muffin.png",
+      "title": "Sausage, Egg & Cheese Muffin",
+      "desc": "290 Cal per sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0018_%5BFeed%5D_0000s_0020_Breakfast_Biscuit.png",
+      "title": "Buttered Biscuit",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Breakfast/EMuffin_PDP.png",
+      "title": "English Muffin",
+      "desc": "Hash Brown Scramble Burrito"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Breakfast/Breakfast%20PDP/_0000s_0009_%5BFeed%5D_0000s_0028_Breakfast_Hashbrowns_2.png",
+      "title": "Hash Browns",
+      "desc": "700 Cal per Burrito"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/Jul19_OrganicAussieGreekYogurtParfaitCupGranola_pdp.png",
+      "title": "Greek Yogurt Parfait",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Sides/Sides%20PDP/_0000s_0010_%5BFeed%5D_0003s_0002_Sides_Fruit-Cup.png",
+      "title": "Fruit Cup",
+      "desc": "Hash Brown Scramble Bowl"
+    }
+],
+entrees:[
+
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/Jul19_CFASandwich_pdp.png",
+      "title": "Chick-fil-A® Chicken Sandwich",
+      "desc": "470 Cal per bowl"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Entree/Entree%20PDP/_0000s_0004_NEWStack6200001CFAPDPDeluxeSandwich1085png.png",
+      "title": "Chick-fil-A® Deluxe Sandwich",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/CFASpicySandwich_1080.png",
+      "title": "Spicy Chicken Sandwich",
+      "desc": "Chicken, Egg & Cheese Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Entree/Entree%20Desktop/_0003s_0012_%5BFeed%5D_0001s_0023_Entrees_Spicy-Deluxe-Sandwich.png",
+      "title": "Spicy Deluxe Sandwich",
+      "desc": "530 Cal per Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Entree/Entree%20PDP/_0000s_0009_Final__0026_CFA_PDP_Grilled-Deluxe-Sandwich_1085.png",
+      "title": "Grilled Chicken Sandwich",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/grilledClub_colbyJack_PDP.png",
+      "title": "Chick-fil-A® Grilled Chicken Club Sandwich",
+      "desc": "Bacon, Egg & Cheese Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/nuggets_8ct_PDP.png",
+      "title": "Chick-fil-A® Nuggets",
+      "desc": "420 Cal per Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/grilledNuggets_8ct_PDP.png",
+      "title": "Grilled Nuggets",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/strips_3ct_PDP.png",
+      "title": "Chick-fil-A Chick-n-Strips®",
+      "desc": "Sausage, Egg & Cheese Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/wrap_pdp.png",
+      "title": "Chick-fil-A® Cool Wrap",
+      "desc": "610 Cal per Biscuit"
+    }
+],
+salad:[
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Salads%26wraps/cobbSalad_nug_pdp.png",
+      "title": "Cobb Salad",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Salads%26wraps/sswSalad_spicyGrilled_pdp.png",
+      "title": "Spicy Southwest Salad",
+      "desc": "Chicken, Egg, & Cheese Muffin"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Salads%26wraps/marketSalad_grilled_pdp.png",
+      "title": "Market Salad",
+      "desc": "420 Cal per Sandwich"
+    }
+  ],
+  sides:[
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/Medium_Lowered_1217shoot_1080x1080.png",
+      "title": "Chick-fil-A Waffle Potato Fries®",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Sides/Sides%20PDP/_0000s_0010_%5BFeed%5D_0003s_0002_Sides_Fruit-Cup.png",
+      "title": "Fruit Cup",
+      "desc": "Bacon, Egg & Cheese Muffin"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/sideSalad_pdp.png",
+      "title": "Side Salad",
+      "desc": "310 Cal per sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Sides/Sides%20PDP/MacnCheese_5oz_pdp.png",
+      "title": "Mac & Cheese",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/Chicken-Noodle-Soup_Small_PDP.png",
+      "title": "Chicken Noodle Soup",
+      "desc": "Sausage, Egg & Cheese Muffin"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Sides/Sides%20Desktop/_0006s_0005_%5BFeed%5D_0003s_0007_Sides_Chicken-Tortilla-Soup.png",
+      "title": "Chicken Tortilla Soup",
+      "desc": "500 Cal per sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/kaleCrunch_sm_pdp.png",
+      "title": "Kale Crunch Side",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/Jul19_OrganicAussieGreekYogurtParfaitCupGranola_pdp.png",
+      "title": "Greek Yogurt Parfait",
+      "desc": "Buttered Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/chips_1080.png",
+      "title": "Waffle Potato Chips",
+      "desc": "290 Cal per Biscuit"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/Buddy_Fruits_PDP.png",
+      "title": "Buddy Fruits® Apple Sauce",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/Dec2019_CFANuggets_5ct_pdp.png",
+      "title": "5 Ct Nuggets Kid's Meal",
+      "desc": "English Muffin"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/strips_2ct_PDP.png",
+      "title": "2 Ct Chick-n-Strips® Kid's Meal",
+      "desc": "140 Cal per muffin"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Entrees/Dec2019_GrilledNuggets_5ct_pdp.png",
+      "title": "5 Ct Grilled Nuggets Kid's Meal",
+      "desc": null
+    }
+  ],
+  treats:[
+
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Treats/Treats%20PDP/_0000s_0003_%5BFeed%5D_0005s_0001_Treats_Frosted-Lemonade.png",
+      "title": "Frosted Lemonade",
+      "desc": "Hash Browns"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Treats/Treats%20Desktop/_0001s_0004_%5BFeed%5D_0005s_0000_Treats_Frosted-Coffee.png",
+      "title": "Frosted Coffee",
+      "desc": "270 Cal per Serving"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/PDP_CCCookie.png",
+      "title": "Chocolate Chunk Cookie",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Treats/Treats%20PDP/031717_FudgeChunkBrownie_PDP.png",
+      "title": "Chocolate Fudge Brownie",
+      "desc": "Greek Yogurt Parfait"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Mobile/Desserts/Menu%20Item/16oz_C%26C_Milkshake-1080.png",
+      "title": "Cookies & Cream Milkshake",
+      "desc": "270 Cal per serving"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Mobile/Desserts/Menu%20Item/16oz_ChocolateMilkshake-1080.png",
+      "title": "Chocolate Milkshake",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Mobile/Desserts/Menu%20Item/16oz_StrawberryMilkshake-1080.png",
+      "title": "Strawberry Milkshake",
+      "desc": "Fruit Cup"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Mobile/Desserts/Menu%20Item/16oz_VanillaMilkshake-1080.png",
+      "title": "Vanilla Milkshake",
+      "desc": "60 Cal per Serving"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Drinks/Drinks%20PDP/_0000s_0027_%5BFeed%5D_0006s_0013_Drinks_Ice-Dream.png",
+      "title": "Chick-fil-A® Icedream® Cone",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Sides%26treats/IceDream_RedCup_1080x1080.png",
+      "title": "Chick-fil-A® Icedream® Cup",
+      "desc": "Chick-fil-A® Chicken Sandwich"
+    }
+  ],
+  drinks:[
+
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/tea_pdp.png",
+      "title": "Freshly-Brewed Iced Tea Sweetened",
+      "desc": "440 Cal per Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/tea_pdp.png",
+      "title": "Freshly-Brewed Iced Tea Unsweetened",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/lemonade_pdp.png",
+      "title": "Chick-fil-A® Lemonade",
+      "desc": "Chick-fil-A® Deluxe Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/lemonade_pdp.png",
+      "title": "Chick-fil-A® Diet Lemonade",
+      "desc": "500 Cal per Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/halfTeahalfLem_pdp.png",
+      "title": "Sunjoy® (1/2 Sweet Tea, 1/2 Lemonade)",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/halfTeahalfLem_pdp.png",
+      "title": "Sunjoy® (1/2 Sweet Tea, 1/2 Diet Lemonade)",
+      "desc": "Spicy Chicken Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/halfTeahalfLem_pdp.png",
+      "title": "Sunjoy® (1/2 Unsweet Tea, 1/2 Lemonade)",
+      "desc": "460 Cal per Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/halfTeahalfLem_pdp.png",
+      "title": "Sunjoy® (1/2 Unsweet Tea, 1/2 Diet Lemonade)",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/2022IcedCoffee_1080x1080.png",
+      "title": "Iced Coffee",
+      "desc": "Spicy Deluxe Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Drinks/Drinks%20PDP/_0000s_0022_Feed_Menu_0000_Drinks_Coca-cola.png",
+      "title": "Coca-Cola®",
+      "desc": "550 Cal per Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Mobile/Beverages/Parent/DrPepper_PaperCup_straight_wet_1200x1200.png",
+      "title": "Dr Pepper®",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Drinks/Drinks%20PDP/_0000s_0026_%5BFeed%5D_0006s_0014_Drinks_Dasani-Water.png",
+      "title": "DASANI® Bottled Water",
+      "desc": "Grilled Chicken Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/COM/Menu_Refresh/Drinks/Drinks%20PDP/_0000s_0017_%5BFeed%5D_appley_620_tiny.png",
+      "title": "Honest Kids® Apple Juice",
+      "desc": "380 Cal per Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/SimplyOJ_Oct19_PDP.png",
+      "title": "Simply Orange®",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Mobile/Kids%20Meals/Parent/220121_Milk_Choc_0009_1080x1080.png",
+      "title": "1% Chocolate Milk",
+      "desc": "Chick-fil-A® Grilled Chicken Club Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Mobile/Kids%20Meals/Parent/220121_Milk_Reg_0009_1080x1080.png",
+      "title": "1% Milk",
+      "desc": "520 Cal per Sandwich"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/Dec19_Coffee_pdp.png",
+      "title": "Coffee",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/lemonadeGallon_PDP2.png",
+      "title": "Gallon Beverages",
+      "desc": "Chick-fil-A® Nuggets"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Drinks/200206_CFA_Icebag_1080x1080.png",
+      "title": "Bag of Ice",
+      "desc": "250 Cal per Entree"
+    }
+  ],
+  drppings:[
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Sauces/sauces_1ozPDP/Sauce_Barbeque_PDP.png",
+      "title": "Barbeque Sauce",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Sauces/sauces_1ozPDP/Sauce_CFA-Sauce_PDP.png",
+      "title": "Chick-fil-A® Sauce",
+      "desc": "Grilled Nuggets"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Sauces/sauces_1ozPDP/Sauce_Garden-Herb-Ranch_PDP.png",
+      "title": "Garden Herb Ranch Sauce",
+      "desc": "130 Cal per Entree"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Sauces/sauces_1ozPDP/Sauce_Honey-Mustard_PDP.png",
+      "title": "Honey Mustard Sauce",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Sauces/sauces_1ozPDP/Sauce_Polynesian_PDP.png",
+      "title": "Polynesian Sauce",
+      "desc": "Chick-fil-A Chick-n-Strips®"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Sauces/sauces_1ozPDP/Sauce_Sweet-and-Spicy-Sriracha_PDP.png",
+      "title": "Sweet and Spicy Sriracha Sauce",
+      "desc": "310 Cal per Entree"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Sauces/sauces_1ozPDP/PDP_1oz-Sauce_Buffalo.png",
+      "title": "Zesty Buffalo Sauce",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Condiments_full-res_Honey-Roasted-BBQ_PDP.png",
+      "title": "Honey Roasted BBQ Sauce",
+      "desc": "Chick-fil-A® Cool Wrap"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Dressing/2ozPDP/2oz-Dressing_full-res_Avocado-Lime-Ranch_PDP.png",
+      "title": "Avocado Lime Ranch Dressing",
+      "desc": "660 Cal per Entree"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Dressing/2ozPDP/2oz-Dressing_full-res_Creamy-Salsa_PDP.png",
+      "title": "Creamy Salsa Dressing",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Dressing/2ozPDP/2oz-Dressing_full-res_Fat-Free-Honey-Mustard_PDP.png",
+      "title": "Fat Free Honey Mustard Dressing",
+      "desc": "Cobb Salad"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Dressing/2ozPDP/2oz-Dressing_full-res_Garden-Herb-Ranch_PDP.png",
+      "title": "Garden Herb Ranch Dressing",
+      "desc": "850 Cal per salad"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Dressing/2ozPDP/2oz-Dressing_full-res_Light-Balsamic-Vinaigrette_PDP.png",
+      "title": "Light Balsamic Vinaigrette Dressing",
+      "desc": null
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Dressing/2ozPDP/2oz-Dressing_full-res_Light-Italian_PDP.png",
+      "title": "Light Italian Dressing",
+      "desc": "Spicy Southwest Salad"
+    },
+    {
+      "image": "https://www.cfacdn.com/img/order/menu/Online/Modifiers/Dressing/2ozPDP/2oz-Dressing_full-res_Zesty-Apple-Cider_PDP.png",
+      "title": "Zesty Apple Cider Vinaigrette Dressing",
+      "desc": "690 Cal per salad"
+    }
+  ]
+}
+console.log(alldata)
+
+  localStorage.setItem("site_data", JSON.stringify(alldata));
+  
+  //console.error("Error saving data:", e);
+
+
+
+// const savedData = localStorage.getItem("site_data");
+// console.log("Saved data:", savedData);
