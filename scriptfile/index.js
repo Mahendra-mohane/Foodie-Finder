b95ae82327e8ea6c8cf8040da577e6a16e2c98b4
Roshan123 <roshan555routh@gmail.com>
2023-02-23 13:43:07 +0530

    day-2

--- scriptfile/index.js
+++ scriptfile/index.js
@@ -1,22 +1,17 @@
 
 let arr=['https://d1fd34dzzl09j.cloudfront.net/Images/CFACOM/Daypart%20Hero/Winter23/Winter23-DotCom-D_710x580-2.1.jpg?h=580&w=710&la=en',
-         'https://st3.depositphotos.com/1000504/17303/i/450/depositphotos_173037454-stock-photo-fresh-tasty-burger.jpg',
-         'https://img.freepik.com/premium-photo/delicious-juicy-burger-isolated-white-background-fast-food_554439-38.jpg?w=900',
+'https://d1fd34dzzl09j.cloudfront.net/Images/CFACOM/Daypart%20Hero/large-CFA2212020-Catering-Daypart-Desktop_710x580V3-1.jpg?h=580&w=710&la=en',
+'https://st3.depositphotos.com/1000504/17303/i/450/depositphotos_173037454-stock-photo-fresh-tasty-burger.jpg',
          'https://img.freepik.com/premium-photo/cheese-burger-with-onion-tomato-lettuce-bacon-white-background_499484-1161.jpg?w=900',
         'https://img.freepik.com/premium-photo/divided-hamburger_147620-1303.jpg?w=740']
-  let hederArray=['Partners in deliciousness','Make it a milkshake break']
+  let hederArray=[`Partners in deliciousness`,`Make it a milkshake break`,`It’ll be our pleasure to serve you again soon`]
         let count=0;
         let count1=0
+        function changeImage()
+        {
 
-        let main=document.querySelector('#slid-image')
-       
-     
-        let hederslid=document.querySelector('#heder')
-        console.log(heder)
-         
-                   function changeImage()
-                     {
-                        hederslid.innertext=hederArray[count1]
+                   let hederslid=document.querySelector('#heder')
+                   hederslid.innertext=hederArray[count1]
                       let img=document.querySelector('#sliding')
                       img.setAttribute("src",arr[count])
                        count++
@@ -27,11 +22,11 @@ let arr=['https://d1fd34dzzl09j.cloudfront.net/Images/CFACOM/Daypart%20Hero/Wint
                          }
                          if(count1==2)
                          {
-                            count=0
+                            count1=0
                          }
                       }
       
-      setInterval(changeImage,4000)
+      setInterval(changeImage,5000)
 
 
 
